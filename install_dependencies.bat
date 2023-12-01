a927f3b44bceab4ef1a6d11b7398fcc2b51d7c79
swedishvegan <swedishvegan@pop-os.localdomain>
2023-12-01 16:36:39 -0500

    git commit

--- install_dependencies.bat
+++ install_dependencies.bat
@@ -3,5 +3,3 @@ vcpkg install glfw3 --triplet x64-windows
 vcpkg install assimp --triplet x64-windows
 vcpkg install freetype --triplet x64-windows
 vcpkg install bullet3 --triplet x64-windows
-cmake . -DCMAKE_TOOLCHAIN_FILE="C:/dev/vcpkg/scripts/buildsystems/vcpkg.cmake"
-cmake --build
---
+++ install_dependencies.sh
@@ -0,0 +1,5 @@
+sudo apt-get install libglew-dev 
+sudo apt-get install libglfw3-dev 
+sudo apt-get install libassimp-dev 
+sudo apt-get install libfreetype-dev 
+sudo apt-get install libbullet-dev 
